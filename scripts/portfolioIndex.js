e0adf72206018209380e177aa18df0fceb0523f9
Cam OD <camryn.odonnell@gmail.com>
2024-06-13 18:59:13 -0400

    Began converting to tailwind, created separate pages, started creating javascript for sort function on portfolio page

---
+++ scripts/portfolioIndex.js
@@ -0,0 +1,11 @@
+let dateSortBtn = document.getElementById("date-sort");
+let nameSortBtn = document.getElementById("name-sort");
+let tagsSortBtn = document.getElementById("tags-sort");
+
+console.log(dateSortBtn);
+console.log(nameSortBtn);
+console.log(tagsSortBtn);
+
+const sortProjects = () => {
+
+};
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'white': '#ffffff',
+        'black': '#0D080E',
+        'red': '#d55130',
+        'blue': '#628bb3',
+        'green': '#826a0d',
+        'yellow': '#e3a327'
+      },
+      fontFamily: {
+        sans: ['IBM Plex Sans', 'Inter', 'Arial','sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+

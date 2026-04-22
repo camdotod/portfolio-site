d9db47c9abf66b5fe453cdf6cdbbe110c8f5d698
Cam OD <camryn.odonnell@gmail.com>
2026-04-22 13:57:37 -0400

    Updated copy for projects entries

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,7 +1,7 @@
 //Project "Database"
 export const projects = [
   {
-    name: "LiUNA Dues Dashboard",
+    name: "LiUNA — Dues Dashboard",
     year: "2025",
     date: "January 2025–April 2025",
     tools: "Figma",
@@ -16,7 +16,7 @@ export const projects = [
     imgstyle: "object-right",
 
     summary:
-      "In partnership with the organization’s admin and developers, I redesigned the payment history page on the member portal to be more clear and transparent about payments and current balances.",
+      "In partnership with the organization’s admin and developers, I redesigned their members’ payment history page to help clear up confusion about overdue payments.",
 
     alt: [
       "Desktop and mobile versions of the dues dashboard screen. The screens are overlapped on top of a green background.",
@@ -26,20 +26,20 @@ export const projects = [
     ],
 
     caption: [
-      "The redesigned Dues Dashboard prioritized responsive design",
-      "Worked with the developer to present data from existing system in a more intuitive way",
-      "Created detailed hand-off documents for implementation.",
+      "The redesign allows members to easily access information about their dues payments from both mobile and desktop devices.",
+      "Previously, the page was unclear about when and how they missed a payment, so the priority was to give members more information while improving readability.",
+      "Detailed specifications for implementation were created in close collaboration with the developer.",
     ],
   },
   {
-    name: "ManeFrame: Social App for Hairstylists",
+    name: "ManeFrame",
     year: "2025",
     date: "July 2024–October 2025",
     tools: "Figma",
     tags: "UX/UI Design",
 
     summary:
-      "Worked on the redesign of the social media app ManeFrame. Led the design direction for feature development.",
+      "Building on existing branding and business goals, I led the UX/UI design for the start-up ManeFrame, a social platform for beauty professionals.",
 
     img: [
       "/assets/ManeFrame/hero_placeholder.png",
@@ -52,13 +52,13 @@ export const projects = [
     alt: ["", "", ""],
 
     caption: [
-      "ManeFrame is a social media app for hairstylists",
-      "A design system was created to ensure consistency across the platform and help with hand-off",
-      "A number of features were redesigned to prepare for the launch of the platform",
+      "The app was designed to help beauty professionals build industry connections, learn new skills, and find community.",
+      "Community and life-long learning are priorities for beauty professionals, so it was important to include industry knowledge and give users different ways to express their identities.",
+      "To ensure consistency in branding and implementation, I developed ManeFrame’s design system in collaboration with the developers.",
     ],
   },
   {
-    name: "Kitchenwares for Canadian Tire",
+    name: "Canadian Tire — Kitchenwares",
     year: "2022",
     date: "August 2021–August 2022",
     tools: "SolidWorks, InDesign, Illustrator, Microsoft Office",
@@ -70,21 +70,25 @@ export const projects = [
       "/assets/Kitchenwares/vida-11lb-5kg-usb-rechargeable-kitchen-scale-2eb3dfe0-7062-4332-b266-b65e898ac3fc.png",
     ],
 
-    imgstyle: "",
+    imgstyle: "object-cover",
 
     summary:
-      "Designed for Canadian Tire's owned brands Paderno and Vida by Paderno. Worked on a variety of ODM products including kitchen appliances, cookware, and tools.",
+      "Developed a number of ODM kitchen products for Canadian Tire’s owned brands Paderno and Vida by Paderno.",
 
-    alt: ["", "", ""],
+    alt: [
+      "Photo of a shiny black hand mixer with stainless steel accents, sitting upright on a white surface. The handle is facing towards the viewer so you can see the speed dial and turbo button. The edges of the turbo button have an orange accent colour. The handle is flat on top with rounded edges and a curved underside and the plastic on the inside of the opening has more of a matte finish. Spiral-shaped dough hooks are attached to the mixer. To the left is a clear plastic storage container that the hand mixer would fit on top of and two sets of attachments: one whisk, and two egg-beaters.",
+      "A series of realistic-looking renderings of the front of a black air fryer. It has a black digital screen and a vertical handle in the center to pull out the basket. There are four renderings total, each with a different design for the icons on the screen and stainless steel accents on the basket.",
+      "Photo of a black digital food scale (left) and black instant-read thermometer (right) staged in a meal-prep scene with food storage containers full of salad and sliced grilled chicken on a cutting board. The scale is flat and rectangular and has a digital screen with white segmented numbers. The scale has a unit button to the left of the screen and a power/tare button to the right. The Vida by Paderno logo, a filled-in circle with a V inside, is printed on the bottom right corner of the scale. The top of the thermometer’s body is circular and has an arrow that follows its circumference to show how the probe rotates open.",
+    ],
 
     caption: [
-      "Vida by Paderno Variable Speed Hand Mixer.",
-      "Mockups of the Vida by Paderno stainless steel air fryer",
-      "I ensured that even low-touch projects fit with the brand's identity.",
+      "While working on the Vida by Paderno Variable Speed Hand Mixer, I wanted to make sure it was easy to hold for extended periods of time. That’s why I designed the handle opening to optimize for balance and added texture to the inner surface for better grip.",
+      "I created a number of technical packages while working with manufacturers, using 2D renderings and CAD models to communicate the design intent.",
+      "I also made sure that products fit with the visual brand language, like with the screen-printed graphics on this instant-read thermometer and scale.",
     ],
   },
   {
-    name: "Counter-cartography of Housing in Ottawa",
+    name: "Counter-cartography of Housing",
     year: "2023",
     date: "February 2023–April 2023",
     tools: "Figma, Illustrator",
@@ -99,27 +103,18 @@ export const projects = [
     imgstyle: "",
 
     summary:
-      "To explore the effects of the financialization of housing in Ottawa I helped plan and facilitate a workshop to engage the people in discussing how this phenomenon affects their relationship to their homes and neighbourhoods.",
+      "To explore the effects of the financialization of housing in Ottawa, I helped plan and facilitate a workshop to discuss how it affects our relationships to our homes and neighbourhoods.",
 
     alt: [
       "An urban skyline showing a condominium under construction.",
-      "A collage of three photos from the workshop activity. The first photo (top left) shows people gathered around a table. On the table, there are houses cut out of construction paper and coloured markers and pens. The second photo (bottom left) shows a three by five grid of question cards that have been filled out. The third photo (right) shows a foam-core board labelled non-ghost with construction paper houses attached to it. The houses have writing on them.",
-      "Two maps of non-ghost and ghost communties. The maps are isometric views of a cartoon city. There are icons of people scattered around the city, with speech bubbles above their heads containg quotes. There is a legend at the bottom explaining the colour-coding of themes and meaning of different icons attached to the quotes.",
+      "A collage of three photos from the workshop activity. The first photo (top left) shows people gathered around a table. On the table, there are houses cut out of construction paper and coloured markers and pens. The second photo (bottom left) shows a three by five grid of question cards that have been filled out. The third photo (right) shows a foam-core board labelled “non-ghost” with construction paper houses attached to it. The houses have writing on them.",
+      "Two maps of non-ghost and ghost communities. The maps are isometric views of a cartoon city. There are icons of people scattered around the city, with speech bubbles above their heads containing quotes. There is a legend at the bottom explaining the colour-coding of themes and meaning of different icons attached to the quotes.",
     ],
 
     caption: [
-      "An urban landscape in Ottawa where some condos with units bought up as investments are known as 'ghost buildings'.",
-      "Participants helped create two cityscapes: one representing a 'ghost' neighbourhood and one representing a 'non-ghost' neighbourhood",
-      "Two 'maps' which represent the",
-      "As housing has become more commodified, some have hypothesized a phenomenon called “ghost buildings”—housing owned only as a financial asset without actual tenants. To investigate the validity and presence of these “ghost buildings”, we reviewed previous research on the subject and observed different neighbourhoods across Ottawa. At the end of our fieldwork, we concluded that perhaps the term “ghost buildings” could be extended to “ghost” neighbourhoods or communities.",
-
-      "Armed with a foundational understanding of the context, we began developing a design workshop as a research tool to capture others’ perceptions of “ghost communities”. We wanted to investigate how “ghost buildings” are created and how they impact their surroundings. We brainstormed activities that would encourage people to share their stories and feelings about their homes and neighbourhoods.",
-
-      "The workshop was conducted with 20 participants. We began by sharing our initial research and understanding of “ghost buildings”. We then asked participants help create a collage of “ghost” and “non-ghost” buildings using their understanding of the concepts. After the activity, question cards were provided for participants to answer in their own time.",
-
-      "Collecting the findings from the workshop, we found recurring themes for “ghost” and “non-ghost” communities. The idea of “ghost” communities evoked feelings of alienation and loss, with themes such as restricted access to spaces, emptiness, lack of utility, and nostalgia. On the other hand, “non-ghost” communities were defined by a sense of activity and interaction and associated with vibrant sensory experiences.",
-
-      "Using Figma and Illustrator, our team transformed the themes and raw data obtained from the workshop into maps illustrating “non-ghost” and “ghost” communities. This counter-cartography was included in a report which documented our research, fieldwork, and implementation of the design workshop. The report was designed to be interactive, with a branching structure to allow readers to delve deeper into certain topics.<br><br> You can view the report here: <a class='underline self-stretch text-wrap break-all' href='https://www.figma.com/proto/haVig1RXgG4R6NquKtZJrV/Counter-Cartographies-Report-(Copy)?node-id=25-53&t=sSG7UoZZLj4npLsq-8&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=25%3A53&hide-ui=1'>Link to Counter Cartographies Report</a>",
+      "The specific term we were interested in was “ghost buildings,” which often describes mostly uninhabited residential buildings that have been bought up as investments.",
+      "We asked participants to help us create two neighbourhoods, one with and one without “ghost buildings”. They each wrote down their thoughts on a building and added it to the collage.",
+      "To visualize our collective understanding of “ghost buildings”, we created two maps. Themes of alienation, emptiness, and nostalgia came up with the ghost buildings, as opposed to activity, interaction, and vibrant sensory experiences.",
     ],
   },
   {
@@ -140,20 +135,21 @@ export const projects = [
     video: "https://www.youtube.com/embed/4WZ-d4zqaCQ?si=DxugqYesuHnydEOL",
 
     summary:
-      "A portable, modular, and customizable system that gives psychotherapists the ability to create a therapeutic environment within any private space",
+      "For my capstone project, I explored ways that access to mental healthcare could be improved through the use of third-spaces. Through my research, I developed a foldable therapy space that lets psychotherapists create a therapeutic environment within any private space.",
 
     alt: [
       "A client and a therapist sitting in a room facing one another. They are surrounded by foldable therapy tiles, which are stacked using interlocking slots to create a space divider around them.",
-      "A mind map of the key stakeholders, sectors, contexts, and trends. Under “stakeholders” are clients and therapists. Under “contexts” are private residences, office buildings, and online. Under “sectors” are public and private. Under “trends” are collaborative care hubs, online & telehealth therapy, and a growing workforce.",
+      "",
+      "",
       "A mind map of the key stakeholders, sectors, contexts, and trends. Under “stakeholders” are clients and therapists. Under “contexts” are private residences, office buildings, and online. Under “sectors” are public and private. Under “trends” are collaborative care hubs, online & telehealth therapy, and a growing workforce.",
       "A collage of three images. The first image (left) is a sketch of a concept with two tent-like structures that lean against each other to form an arch over the client and therapist. The second image (center) is a photograph from a side angle of a person sitting in a chair beneath a full-scale model of the previous concept. The third image (right) is a comic/narrative prototype showing a therapist bringing the device to a coworking space, setting it up, and taking it apart.",
       "A collage of images showing cardboard prototypes, a moodboard, and a 3D rendering of the final concept: foldable tiles that can be assembled by stacking them on top of one another in different configurations.",
     ],
 
     caption: [
-      "The tiles can be set up in a number of combinations to change how the space feels",
-      "Day to day, this would enable therapists to work in third spaces with private rooms.",
-      "Here's a video of how this might work",
+      "The product is a portable, modular, and customizable system of interlocking tiles, made from recycled PET board.",
+      "In a place like a soundproof meeting room or doctor’s office, the boundary created by the tiles dispels the sense of exposure and austerity that previously prevented therapists & clients from feeling comfortable in these spaces.",
+      "Therapists can easily transport and set-up the foldable therapy space, allowing it to fit seamlessly into their busy schedules.",
     ],
   },
 
@@ -166,24 +162,25 @@ export const projects = [
 
     img: [
       "/assets/Early/Early-Image_0.png",
-      "/assets/Early/Early-Image_2.png",
+      "/assets/Early/JourneyMap-Section3.png",
       "/assets/Early/Early-Image_5.png",
     ],
 
-    imgstyle: "object-right",
+    imgstyle: "object-left",
 
     summary:
-      "An app to take the stress out of the daily commute for Ottawa transit riders.",
+      "Along with a team, I helped design EARLY, an app to take the stress out of the daily commute for Ottawa transit riders.",
 
     alt: [
-      "User persona of Suzie Choi, 21 year old student and sales associate. Image text: Bio: Suzie is a 4th-year international student taking Computer Science at Carleton University. She works part-time at the Rideau Centre and has a busy schedule between school and work. She doesn’t have a licence and wants to save money so she relies heavily on the bus. Apps Used: Google Maps,Transit. Goals: plan her bus routes before her trips, be on schedule for work and school, secure personal space on the bus, be COVID-safe on the bus, save money by making use of her UPass. Pain Points: stressed about buses being potentially late, GPS tracking disconnecting, delayed buses making her late, crowded buses and safety measures not being practiced, concerned about the cleanliness of bus seats, handles, and buzzers. Quote: “I always leave early because I can’t fully trust the bus schedule or GPS tracking.”.",
+      "A graphic showing the EARLY’s logo and screens. The app’s logo is a red arrow that loops around to create the letter E. The screens shown are previews of the app’s home page and navigation interface.",
+      "A segment of a journey map, which shows what a commuter might do, think, feel, and interact with while taking the bus home from work. The journey map shows the commuter using Google Maps and Transit to check for her bus, anxious about when it’s going to come. Once the commuter gets on the bus, she feels relieved, but begins stressing again thinking about their next transfer so she checks Transit again. At the transfer stop, she’s frustrated and dismayed when she sees her bus leave just as she’s arriving. She checks Transit again and waits for the next one. Finally after catching the bus, she arrives home.",
       "Three screens showcasing key design features of the EARLY app. Text for the home screen: “Daily commute forecast: incorporates weather and traffic for a complete view of your commute. Favourite Routes: through a simple set-up process, users can input information about their daily commute to get notifications and updates on the status of most-used bus routes”. Text for the search details: “Planning Tools: Route information is structured to allow new users to learn about Ottawa’s transit system while giving experienced users the most relevant information. Bus Station Info: Bus stations have additional information to help users navigate their transfers.” Text for the view of the current trip” Trip Tracking: Using a combination of official OC Transpo data, traffic data, and crowdsourced information on delays and crowdedness, EARLY provides users with the most relevant & accurate information possible. See Alternate Routes:Allows users to quickly switch routes in case of last minute delays or obstacles.”",
     ],
 
     caption: [
-      "Early's branding and screens",
-      "Its design centered around what we learned from talking to commuters. This was a persona created to represent who our end user was.",
-      "Early helps commuters stay on top of their schedule by learning their frequent routes, providing up-to date transit information, and allowing for on-the-go changes to navigation",
+      "EARLY is an all-in-one transit tracking and navigation app that attunes to your daily routine and provides on-the-go updates to help deal with unexpected delays.",
+      "After interviewing a number of transit users, we learned that frequent commuters often used a number of apps to assist them, but were still left stressing during transfers or when their regular route was detoured.",
+      "We wanted to streamline this experience and take a more proactive-approach to transit-tracking. By learning the user’s commute, EARLY provides alerts before you even leave the house. And with detailed transit information, riders can easily adapt to any obstacle they face.",
     ],
   },
 ];

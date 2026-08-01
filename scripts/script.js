ecc6126c585ab3b7b80f08c89a673512cfef73ba
Cam OD <camryn.odonnell@gmail.com>
2026-08-01 11:58:41 -0400

    Fixed z-index of header

--- scripts/script.js
+++ scripts/script.js
@@ -44,7 +44,7 @@ function setLayout() {
  * @returns HTML Element
  */
 function addHeader() {
-  addClasses(header, "bg-bg-color sticky md:relative top-0 z-10 w-full");
+  addClasses(header, "bg-bg-color sticky md:relative top-0 z-20 w-full");
 
   var folder = ".";
   if (url.indexOf("portfolio/") > -1) {
@@ -66,7 +66,7 @@ function addHeader() {
       </button>
     </div>
     <div id="nav-links"
-      class="bg-bg-color z-10 font-display absolute w-full flex-col border-t border-b px-6 py-6 md:pr-60 md:pl-60 lg:relative lg:flex lg:w-auto lg:flex-row lg:gap-6 lg:border-0 lg:py-0 lg:pb-6 lg:pl-0 hidden">
+      class="bg-bg-color z-20 font-display absolute w-full flex-col border-t border-b px-6 py-6 md:pr-60 md:pl-60 lg:relative lg:flex lg:w-auto lg:flex-row lg:gap-6 lg:border-0 lg:py-0 lg:pb-6 lg:pl-0 hidden">
       <a id="link-portfolio"
         class="focus-visible:outline-fg-color flex h-10 w-fit items-center underline underline-offset-2 decoration-fg-color/50 focus-visible:outline-1 focus-visible:outline-offset-4 focus-visible:outline-solid active:opacity-80 hover:decoration-fg-color focus-visible:decoration-fg-color rounded-xl active:decoration-fg-color"
         href="${folder}/portfolio.html" tabindex="0"><span>portfolio</span></a>
